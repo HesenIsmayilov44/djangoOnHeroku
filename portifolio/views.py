57696f2a0ada14b74cf3ac1c18c5e3252f54f984
Həsən İsmayılov <hesenismayilov44@gmail.com>
2021-03-01 21:20:23 +0400

    Start create site

--- portifolio/views.py
+++ portifolio/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Birincisi Salam')
+    return HttpResponse('<h1>Birincisi Salam :)</h1>')
