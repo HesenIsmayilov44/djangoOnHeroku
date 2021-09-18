be4b1f405c6217e7a755cc779bc7899829f0c2f6
Həsən İsmayılov <hesenismayilov44@gmail.com>
2021-09-18 19:18:24 +0400

    static files and postegros

--- djangoonheroku/settings.py
+++ djangoonheroku/settings.py
@@ -136,8 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
