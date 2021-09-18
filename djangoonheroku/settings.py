b4b61027bc161c303e48b9deb021609530ae3669
Həsən İsmayılov <hesenismayilov44@gmail.com>
2021-09-18 22:00:12 +0400

    change static root name

--- djangoonheroku/settings.py
+++ djangoonheroku/settings.py
@@ -138,8 +138,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
