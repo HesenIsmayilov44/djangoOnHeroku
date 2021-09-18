3a039b1c81b9f75e5e29cde3f94a15f1a460c153
Həsən İsmayılov <hesenismayilov44@gmail.com>
2021-09-18 22:16:12 +0400

    test

--- djangoonheroku/settings.py
+++ djangoonheroku/settings.py
@@ -138,9 +138,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [ 
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [ 
+#     BASE_DIR / 'static'
+# ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
