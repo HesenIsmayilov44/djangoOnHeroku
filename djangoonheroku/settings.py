556e665e17b4c093a66fc6b7a9af18b2f65bca62
Həsən İsmayılov <hesenismayilov44@gmail.com>
2021-09-18 15:32:43 +0400

    added hosts

--- djangoonheroku/settings.py
+++ djangoonheroku/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'nhbs+$o7@%yu&y4n4rehh4+cfzk8*2oq0^ukgj8jbn*06c-4cn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['hasanismayilov.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['134.122.78.246']
 
 
 # Application definition
